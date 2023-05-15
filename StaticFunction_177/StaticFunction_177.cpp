57d228e23d50297128187bccb30b6ca9bdceac48
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-05-15 15:40:55 +0700

    2

--- StaticFunction_177/StaticFunction_177.cpp
+++ StaticFunction_177/StaticFunction_177.cpp
@@ -16,9 +16,38 @@ public :
 int mahasiswa::nim = 0;
 
 void mahasiswa::setID() {
-	id = ++nim;
+	id = ++nim;										// penggunaan nim = 1 + nim, memakai 1 dulu baru ditambahkan dengan nilai nim
 }
 
 void mahasiswa::printAll() {
 	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
 }
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+
+	/*mhs1.nim = 20;*/				//bisa di atur jumlah nim di int main, di bawahnya akan menjadi 21 dikarenakan ++nim
+
+	mahasiswa mhs2("Asroni");
+
+	/*mhs2.nim = 30;*/				//di bawahnya akan menjadi 31 dikarenakan ++nim
+
+	mahasiswa mhs3("Andi Kurniawan");
+
+	/*mhs3.nim = 40;*/				//di bawahnya akan menjadi 41 dikarenakan ++nim
+
+	mahasiswa mhs4("Joko Purbo");
+
+	/*mhs4.nim = 50;*/				//di bawahnya akan menjadi 51 dikarenakan ++nim
+
+
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
+}
+
