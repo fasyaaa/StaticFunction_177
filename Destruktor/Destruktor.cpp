4d97efa0e123e3a28b100a601db2d174a609285d
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-05-15 19:26:01 +0700

    konstruktor

---
+++ Destruktor/Destruktor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class angka {
+private :
+	int* arr;
+	int panjang;
+public :
+	angka(int);				//Constructor
+	~angka();
+};
--- konstructor/konstructor.cpp
+++ konstructor/konstructor.cpp
@@ -2,3 +2,50 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+private :
+	int nim;
+	string nama;
+public : 
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
+};
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+void mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Fasya");			//bagian nim akan berisi - bla bla bla dikarenakan tidak diberi nim = 0
+	mahasiswa mhs4(30, "Dimas");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
