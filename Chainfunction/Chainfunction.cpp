d44bc25c18841c81b352e2f3986329d867fc5c60
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-05-15 19:40:04 +0700

    chainfunction

---
+++ Chainfunction/Chainfunction.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class buku {
+	string judul;
+public :
+	buku setJudul(string judul) {
+		this->judul = judul;
+		return *this;							//chain function
+	}
+	string getJudul() {
+		return this->judul;
+	}
+} bukunya;
+
+int main() {
+	//bukunya.setJudul("Matematika");						//Chain function calls, pemanggilan dengan cara 1
+	//cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul();		//Chain function calls, pemanggilan dengan cara 2
+	return 0;
+}
